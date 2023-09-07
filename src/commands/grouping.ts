6128cff4b338779ff6f0633b18d2adfb11d2b1dc
MnyaCat <mnnk.1314@gmail.com>
2023-09-08 02:53:05 +0900

    excludeオプションを追加

--- src/commands/grouping.ts
+++ src/commands/grouping.ts
@@ -52,6 +52,13 @@ export class GroupingCommand extends Command {
                             .setDescription(
                                 "1つのグループに参加できる最大のユーザー数"
                             )
+                    )
+                    .addStringOption((option) =>
+                        option
+                            .setName("exclude")
+                            .setDescription(
+                                "グループ分けの対象に含めないメンバー。メンションで指定してください。"
+                            )
                     ),
             { idHints: idHints }
         );
@@ -61,6 +68,7 @@ export class GroupingCommand extends Command {
         interaction: Command.ChatInputCommandInteraction
     ) {
         const maxGroupSize = interaction.options.getNumber("groupsize") ?? 4;
+        const exclude = interaction.options.getString("exclude") ?? "";
 
         if (maxGroupSize < 2) {
             const embed = errorEmbed(
@@ -68,6 +76,10 @@ export class GroupingCommand extends Command {
             );
             return interaction.reply({ embeds: [embed], ephemeral: true });
         }
+
+        const excludeMemberIds = [
+            ...exclude.matchAll(/<@!?(?<id>\d{17,20})>/g),
+        ].map((value) => value[1]);
 
         let voiceChannel: VoiceBasedChannel;
         try {
@@ -82,17 +94,31 @@ export class GroupingCommand extends Command {
             }
         }
 
-        const memberMentions = voiceChannel.members.map(
-            (member) => `<@${member.id}>`
+        const voiceChannnelMembers = voiceChannel.members.map(
+            (member) => member.id
         );
+        const includeMemberMentions = [];
+        for (let i = 0; i < voiceChannnelMembers.length; i++) {
+            const memberId = voiceChannnelMembers[i];
+            if (!excludeMemberIds.includes(memberId)) {
+                includeMemberMentions.push(`<@${memberId}>`);
+            }
+        }
+        if (includeMemberMentions.length < 1) {
+            const embed = errorEmbed(
+                "グループ分けの対象メンバーが1人未満です。`exclude`オプションを変更してください。"
+            );
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
         // メンバー数をできる限り揃えるために調整
         const groupSize =
-            memberMentions.length /
-            Math.ceil(memberMentions.length / maxGroupSize);
+            includeMemberMentions.length /
+            Math.ceil(includeMemberMentions.length / maxGroupSize);
         const groups =
-            memberMentions.length >= groupSize
-                ? grouping(memberMentions, groupSize)
-                : [[...memberMentions]];
+            includeMemberMentions.length >= groupSize
+                ? grouping(includeMemberMentions, groupSize)
+                : [[...includeMemberMentions]];
 
         const embed = new EmbedBuilder()
             .setTitle("グループ分けの結果")
