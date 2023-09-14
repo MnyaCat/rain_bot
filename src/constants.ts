e07181b21be40bb14cc101414f2ddc6f5c892e18
MnyaCat <mnnk.1314@gmail.com>
2023-09-14 22:13:01 +0900

    ボタンのIDに使う値を追加

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
 export const errorColor = 0xf23f42;
 export const mcColor = 0x6bc349;
+export const rerollButtonIds = {
+    weapon: "reroll-weapon",
+    mainWeapon: "reroll-mainweapon",
+    subWeapon: "reroll-subweapon",
+    specialWeapon: "reroll-specialweapon",
+};
