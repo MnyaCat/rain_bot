aa64a625e5d81dbf1a65d81a9e13858b15bae7a0
MnyaCat <mnnk.1314@gmail.com>
2024-02-01 12:13:02 +0900

    remove: generateItemNotFoundErrorEmbedを削除。各コマンドごとに関数を分けたため。

--- src/utils/embed_builder.ts
+++ src/utils/embed_builder.ts
@@ -1,7 +1,6 @@
 import { EmbedBuilder } from "discord.js";
 import { errorColor } from "../constants";
 import { container } from "@sapphire/framework";
-import { RandomCommandOptions } from "../commands/random";
 import {
     RandomSpecialWeaponElementNotFoundError,
     RandomStageElementNotFoundError,
@@ -14,66 +13,6 @@ export function buildErrorEmbed(description?: string) {
         .setColor(errorColor)
         .setTitle("エラーが発生しました");
     return description ? embed.setDescription(description) : embed;
-}
-
-export async function generateItemNotFoundErrorEmbed(
-    options: RandomCommandOptions
-) {
-    const subWeaponId = options.subWeaponId;
-    const specialWeaponId = options.specialWeaponId;
-    const seasonId = options.seasonId;
-    const weaponTypeId = options.weaponTypeId;
-
-    const prisma = container.database;
-    const subWeaponFilter =
-        subWeaponId != null
-            ? await prisma.subWeapon.findFirst({
-                  where: { id: subWeaponId },
-              })
-            : null;
-    const specialWeaponFilter =
-        specialWeaponId != null
-            ? await prisma.specialWeapon.findFirst({
-                  where: { id: specialWeaponId },
-              })
-            : null;
-    const seasonFilter =
-        seasonId != null
-            ? await prisma.season.findFirst({ where: { id: seasonId } })
-            : null;
-    const weaponTypeFilter =
-        weaponTypeId != null
-            ? await prisma.weaponType.findFirst({
-                  where: { id: weaponTypeId },
-              })
-            : null;
-
-    const filters = [
-        subWeaponFilter,
-        specialWeaponFilter,
-        seasonFilter,
-        weaponTypeFilter,
-    ];
-
-    const filterNames = [
-        "サブウェポン",
-        "スペシャルウェポン",
-        "シーズン",
-        "ブキタイプ",
-    ];
-
-    const filtersTexts = [];
-    for (let i = 0; i < filters.length; i++) {
-        const filter = filters[i];
-        const filterName = filterNames[i];
-        if (filter != null) {
-            filtersTexts.push(`- ${filterName}: ${filter.name}`);
-        }
-    }
-
-    return buildErrorEmbed(
-        "以下の条件に合うアイテムがありません。\n\n" + filtersTexts.join("\n")
-    );
 }
 
 export async function buildRandomWeaponElementNotFoundEmbed(
