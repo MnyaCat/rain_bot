97967e8b9012694aa2ce81b0b433bc0a2337c810
MnyaCat <mnnk.1314@gmail.com>
2024-02-07 15:09:58 +0900

    change: randomSubweaponに依存するように変更、ボイスチャンネルに参加していなくても実行できるように変更

--- src/interaction-handlers/reroll_subweapon.ts
+++ src/interaction-handlers/reroll_subweapon.ts
@@ -5,12 +5,16 @@ import {
 } from "@sapphire/framework";
 import type { ButtonInteraction } from "discord.js";
 import { rerollButtonIds } from "../constants";
-import { RandomCommand, RandomSubWeaponOptions } from "../commands/random";
 import {
     checkCustomId,
-    checkVoiceChannelJoining,
     getExecutedMember,
+    isVoiceChannelJoinig,
 } from "../utils/utils";
+import { MemberVoiceChannelNotJoining } from "../errors";
+import {
+    RandomSubWeaponOptions,
+    buildRandomSubWeaponResult,
+} from "../commands/randomSubweapon";
 
 @ApplyOptions<InteractionHandler.Options>({
     interactionHandlerType: InteractionHandlerTypes.Button,
@@ -20,13 +24,19 @@ export class ButtonHandler extends InteractionHandler {
         interaction: ButtonInteraction,
         parsedData: InteractionHandler.ParseResult<this>
     ) {
-        const member = await getExecutedMember(interaction);
-        checkVoiceChannelJoining(member);
         const options = parsedData.options;
-        const replyOptions = await RandomCommand.buildRandomSubWeaponResult({
+        const executedMember = await getExecutedMember(interaction);
+        const voiceChannelJoining = isVoiceChannelJoinig(executedMember);
+        // ボイスチャンネルに参加していない状態でonlyOneがfalseの再ロールボタンを押した場合に例外をスローする
+        if (!options.onlyOne && !voiceChannelJoining) {
+            throw new MemberVoiceChannelNotJoining(
+                "**[1つのみ抽選する]**が有効になっていないため、ボイスチャンネルに参加していない状態では再ロールできません。"
+            );
+        }
+        const replyOptions = await buildRandomSubWeaponResult({
             interaction: interaction,
             seasonId: options.seasonId,
-            single: options.single,
+            onlyOne: options.onlyOne,
             timestamp: true,
         });
         await interaction.update(replyOptions);
